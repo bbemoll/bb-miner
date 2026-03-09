ead64899e2fd8f33ca7faa42d62dcc3895c1734b
bbemoll <dev@bemoll.com>
2026-03-09 02:28:02 +0100

    fixed small bugs

--- custom_components/miner/number.py
+++ custom_components/miner/number.py
@@ -128,12 +128,12 @@ class MinerPowerLimitNumber(CoordinatorEntity[MinerCoordinator], NumberEntity):
             )
 
 # EBE_20260309_BEGIN
-        result = False
-#        result = await miner.set_power_limit(int(value))
+#        result = False
+        result = await miner.set_power_limit(int(value))
         _LOGGER.warning(
             f"EBE_20260309_51: number.py async_set_native_value: {value}")
 
-        # EBE_20260309_BEGIN
+# EBE_20260309_END
 
         if not result:
             raise pyasic.APIError("Failed to set wattage.")
